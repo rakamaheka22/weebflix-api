429f0e878e26d70f23d4f87e203096d62fefa0bb
Raka Maheka Auramuwahid <rakamaheka@gmail.com>
2021-10-02 12:26:02 +0700

    initial project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+import firebase from 'firebase/compat/app';
 import App from './App.vue';
 import router from './router';
+
+const {
+  VUE_APP_API_KEY,
+  VUE_APP_AUTH_DOMAIN,
+  VUE_APP_PROJECT_ID,
+  VUE_APP_STORAGE_BUCKET,
+  VUE_APP_MESSAGING_SENDER_ID,
+  VUE_APP_APP_ID,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: VUE_APP_API_KEY,
+  authDomain: VUE_APP_AUTH_DOMAIN,
+  projectId: VUE_APP_PROJECT_ID,
+  storageBucket: VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: VUE_APP_MESSAGING_SENDER_ID,
+  appId: VUE_APP_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,119 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <button @click="doRegisterAccount">
+      Register
+    </button>
+
+    <button @click="doLogin">
+      Login
+    </button>
+
+    <button @click="doLogout">
+      Logout
+    </button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  mounted() {
+    // this.fetchListFilms();
+    // this.fetchData();
+    // this.asinkronKode();
+    // this.hitApi();
+    firebase.auth().onAuthStateChanged((res) => {
+      if (res) console.log(true);
+      else console.log(false);
+    });
+  },
+  methods: {
+    async fetchListFilms() {
+      try {
+        const { data } = await this.axios.get('https://ghibliapi.herokuapp.com/films');
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async fetchData() {
+      try {
+        const response = await fetch('https://ghibliapi.herokuapp.com/people');
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    inputData() {
+      setTimeout(() => {
+      }, 1000);
+      const input = 'INPUT DATA';
+      return input;
+    },
+    async asinkronKode() {
+      console.log('Contoh Syncrounous');
+      console.log('START');
+      const input = await this.inputData();
+      console.log(input);
+      console.log('END');
+
+      console.log('Contoh Asyncrounous');
+      console.log('START');
+      setTimeout(() => {
+        console.log('INPUT DATA');
+      }, 1000);
+      console.log('END');
+    },
+    hitApi() {
+      const data = [1, 2, 3, 4, 5];
+
+      const api = new Promise((resolve, reject) => {
+        // janji untuk memberikan data
+        if (data && data.length > 0) {
+          resolve(data);
+        }
+        reject(new Error('tidak ada data'));
+      });
+
+      api.then((response) => {
+        console.log(response);
+      }).catch((error) => console.error(error));
+    },
+    doRegisterAccount() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword('fulan2@gmail.com', 'rahasia')
+        .then((res) => {
+          console.log(res);
+        }).catch((err) => console.error(err));
+    },
+    doLogin() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword('fulan2@gmail.com', 'rahasia')
+        .then((res) => {
+          console.log(res);
+        }).catch((err) => console.error(err));
+    },
+    doLogout() {
+      firebase
+        .auth()
+        .signOut()
+        .then((res) => {
+          console.log(res);
+        }).catch((err) => console.error(err));
+    },
   },
 };
 </script>
